2be6f31f177762ea45a74cf14b27897c23e9c18b
Tim Budden <tim.budden@datasift.com>
2016-01-02 23:50:45 +0000

    added ipynb using PCA to gauge cost complexity of BNF chapters; improved BNF decoder; plus few other tweaks

--- gbq.py
+++ gbq.py
@@ -1,4 +1,6 @@
-import subprocess,time
+import subprocess,time,sys
+import hashlib
+import os.path
 try: 
     import ujson as json
 except:
@@ -17,25 +19,75 @@ For it to work, you must first configure the environment for bq using the gcloud
 
 '''
 
-def query(sql,max_rows=100,cast={},index=None):
-    t0=time.time()
-    args = ['bq','--format=json','query','--max_rows=%i' % max_rows ,sql]
-    p=subprocess.Popen(args,stdout=subprocess.PIPE)
-    (out,err)=p.communicate()
-    if not err:
-        for i in out.split('\n'):
-            try:
-                j=json.loads(i)
-                df=pd.DataFrame(j)
-                for k,func in cast.items():
-                  if k in df.columns:
-                    df[k]=df[k].apply(func)
-                if index:
-                  df.set_index(index,inplace=True)
-                print "%i rows in %.2f seconds" % (len(df),time.time()-t0)
-                return df
 
-            except Exception,e:
-                print i
+def read_from_cache(sql,cache):
+    sqlhash=hashlib.md5(sql).hexdigest()
+    os.path.isfile(fname) 
+
+def query_():
+
+    if cache and not dirty:
+        pass
+        #read the cache
     else:
-        print err
+        pass
+        # query gbq
+        #
+
+    if cache and (dirty): #or not fileexists!!!:
+        pass
+        #write the cache
+
+    #convert to df
+
+
+def to_df_(j,cast,index):
+    t0=time.time()
+    df=pd.DataFrame(j)
+    for k,func in cast.items():
+      if k in df.columns:
+        df[k]=df[k].apply(func)
+    if index:
+      df.set_index(index,inplace=True)
+    print "%i rows converted to DataFrame in %.2f seconds" % (len(df),time.time()-t0)
+    return df
+
+
+def query(sql,max_rows=100,cast={},index=None,cachedir=None,dirty=False):
+
+    if dirty and cachedir is None:
+        print "Need to specify cachedir if dirty flag set"
+        return
+
+    if cachedir:
+        cachefile=os.path.join(cachedir,hashlib.md5(sql).hexdigest()+'.json')
+
+    if cachedir is None or dirty or not os.path.exists(cachefile):
+        t0=time.time()
+        args = ['bq','--format=json','query','--max_rows=%i' % max_rows ,sql]
+        p=subprocess.Popen(args,stdout=subprocess.PIPE)
+        (out,err)=p.communicate()
+        if not err:
+            t1=time.time()
+            print "Big Query returned in %.2f seconds" % (t1-t0)
+            sys.stdout.flush()
+            for i in out.split('\n'):
+                try:
+                    j=json.loads(i)
+                    df=to_df_(j,cast,index)
+                    if cachedir:
+                        with open(cachefile,'w') as f:
+                            json.dump(j,f)
+                    return df
+                except Exception,e:
+                    print i
+        else:
+            print err
+
+    else:
+        print "Reading cached results from", cachefile
+        with open(cachefile,'r') as f:
+            j=json.load(f)
+            df=to_df_(j,cast,index)
+            return df
+
